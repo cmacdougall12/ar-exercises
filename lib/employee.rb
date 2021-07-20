1470061731ba69a07f3e07aa84703c0335141ab0
Cam MacDougall <cammacdougall12@gmail.com>
2021-07-20 14:31:09 +0000

    ex7 complete

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates_numericality_of :hourly_rate, only_integer: true
+  validates_numericality_of :hourly_rate, greater_than: 60
+  validates_numericality_of :hourly_rate, less_than: 200
 end
