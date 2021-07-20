36d1d2b376c9b292b90f442c2bb114351daf7935
Cam MacDougall <cammacdougall12@gmail.com>
2021-07-20 01:05:42 +0000

    ex5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum("annual_revenue");
+average_revenue = Store.average("annual_revenue")
+rich_stores = Store.where("annual_revenue> 150000").count
+
+
+pp "Total Revenue: #{total_revenue}"
+pp "Average Revenue: #{average_revenue.to_i}"
+pp "There are #{rich_stores} making more than $150000"
